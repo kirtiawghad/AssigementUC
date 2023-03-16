461305367c7a37b42dbd05bc05c91f9140822762
kirtimadam <104613258+kirtimadam@users.noreply.github.com>
2023-03-16 18:35:08 +0530

    casestatementemployeewage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -57,3 +57,21 @@ else
 fi
 
 
+isparttime=1;
+isfulltime=2;
+emprateperhr=20;
+empcheck=$((RANDOM%3));
+
+case $empcheck in
+      $isfulltime)
+      emphrs=8
+      ;; 
+     $isparttime)
+      emphrs=4
+      ;;
+*)
+      emphrs=0
+      ;;
+esac
+salary=$(($emphrs*$emprateperhr));
+
