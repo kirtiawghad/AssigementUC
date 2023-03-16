f498c7cbd75a22b7d64a65cd362bd902b873281e
kirtimadam <104613258+kirtimadam@users.noreply.github.com>
2023-03-16 15:35:21 +0530

    emplyeeAttendence

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,16 @@
-# ! bin / bash
+#!/bin/bash 
+
 echo "Wellcome to Employee Wage Computation Program"
+ 
+ ispresent=1;
+ randomcheck=$((random%2));
+if [ $ispresent -eq $randomcheck ];
+then 
+
+echo "Employee is precent";
+else
+
+echo "Employee is not precent";
+
+fi
+
